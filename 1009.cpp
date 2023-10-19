1f2c2e8069ff128a55f8422b62d746dd32a0c290
Igor Gabriel <131968406+Igor1208gabriel@users.noreply.github.com>
2023-10-19 11:29:29 -0300

    add arquivos pc lab6
    finalizar 1245

---
+++ 1009.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){ 
+    string nome; cin >> nome;
+    
+    int fi; cin >> fi;
+    int xo; cin >> xo;
+
+    int comi; cin >> comi;
+    int ssao; cin >> ssao;
+
+    cout << fixo << " " << comissao << comi;
+}
