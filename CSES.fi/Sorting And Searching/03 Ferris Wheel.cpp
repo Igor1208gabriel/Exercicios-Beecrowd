81e5ce62e033fa1848df645901b77949c4e681cf
Igor Gabriel <131968406+Igor1208gabriel@users.noreply.github.com>
2023-11-08 13:11:51 -0300

    Update 03 Ferris Wheel.cpp

--- CSES.fi/Sorting And Searching/03 Ferris Wheel.cpp
+++ CSES.fi/Sorting And Searching/03 Ferris Wheel.cpp
@@ -2,37 +2,29 @@
 using namespace std;
 
 int main(){
-    int n_criancas, max_gondola; cin >> n_criancas >> max_gondola;
-    int criancas[n_criancas];
-
-    for(int i = 0; i < n_criancas; i++){
+    int nc, pm; cin >> nc >> pm;
+    int x = nc;
+    vector<int> lc;
+    while(x--){
         int temp; cin >> temp;
-        criancas[i] = temp;
-    }
+        lc.push_back(temp);
+    } 
+    sort(lc.begin(), lc.end());
 
-    sort(criancas, criancas+n_criancas);
-    int count = 0;
 
-    int i = 0, j = n_criancas-1; 
-    bool conseguiu[n_criancas];
-
-    while(i < j){
-        if (criancas[i]+criancas[j] > max_gondola) j--;
-        else {
+    int i = 0, j = nc-1, count = 0;
+    while(i <= j){
+        if(lc[i] + lc[j] <= pm){
             count++;
-            conseguiu[i] = conseguiu[j] = true;
-            j--;
             i++;
-            }
-        count++;
-    }
-
-    for(int i = 0; i < n_criancas; i++){
-        count += conseguiu[i] == false;
+            j--;
+        }
+        else{
+            count++;
+            j--;
+        }
     }
 
     cout << count << "\n";
-} 
-    }
-    cout << ans;
 }
+
